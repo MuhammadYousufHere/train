4c85caaa3c94deb901b35909f9f40eb15333d7e9
MuhammadYousufHere <khanyousufmy80@gmail.com>
2022-11-14 22:48:44 +0500

    sidebar-fixes and scroll

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ErrorBoundary from "./common/ErrorBoundary";
-import Navbar from "./components/Navbar/Navbar";
-import ScrollToTop from "./components/ScrollTop";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import FAQ from "./pages/FAQ";
-import Footer from "./pages/Footer";
-import Home from "./pages/Home";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ErrorBoundary from './common/ErrorBoundary';
+import ScrollPageToTop from './common/ScrollPageToTop';
+import Navbar from './components/Navbar/Navbar';
+import ScrollToTop from './components/ScrollTop';
+import Sidebar from './components/Sidebar/Sidebar';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+import FAQ from './pages/FAQ';
+import Footer from './pages/Footer';
+import Home from './pages/Home';
 function App() {
   const [showSidebar, setShowSidebar] = useState(false);
   const handleSidebar = () => {
@@ -18,15 +19,28 @@ function App() {
     <Router>
       <ErrorBoundary>
         <ScrollToTop smooth />
+        <ScrollPageToTop />
         <Navbar onShowSidebar={handleSidebar} />
         {showSidebar && <Sidebar />}
 
         <div className='app'>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/faq' element={<FAQ />} />
-            <Route path='/blogs' element={<Blogs />} />
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/contact'
+              element={<Contact />}
+            />
+            <Route
+              path='/faq'
+              element={<FAQ />}
+            />
+            <Route
+              path='/blogs'
+              element={<Blogs />}
+            />
           </Routes>
         </div>
         <Footer />
