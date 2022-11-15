77cebec511f1b398e458836ad6814d65eaf5f160
MuhammadYousufHere <khanyousufmy80@gmail.com>
2022-11-15 22:24:15 +0500

    header theme

--- src/pages/ShowCase/Showcase.js
+++ src/pages/ShowCase/Showcase.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Card from "./Card";
-import { data } from "./data";
+import React from 'react';
+import Card from './Card';
+import { data } from './data';
 const Showcase = () => {
   return (
-    <section className="showcase-container">
-      <div className="showcase-body">
+    <section className='showcase-container'>
+      <div className='showcase-body'>
         {data.map((item) => (
           <Card
             key={item.id}
-            icon={item.icon}
+            icon={item.icons_2}
             tag_1={item.tag_1}
             tag_2={item.tag_2}
             text_1={item.details}
--- src/pages/ShowCase/data.js
+++ src/pages/ShowCase/data.js
@@ -1,28 +1,34 @@
-import ticket from "../../assets/ticket-icon.png";
-import train from "../../assets/traver-icon.png";
-import wallet from "../../assets/wallet.png";
+import ticket from '../../assets/ticket-icon.png';
+import train from '../../assets/traver-icon.png';
+import wallet from '../../assets/wallet.png';
+import pass from '../../assets/pass.png';
+import offer from '../../assets/offer.png';
+import pay from '../../assets/pay.png';
 
 export const data = [
   {
     id: 1,
-    tag_1: "Fastest Train",
-    tag_2: "Ticketing Solutions",
+    tag_1: 'Fastest Train',
+    tag_2: 'Ticketing Solutions',
     icon: train,
+    icons_2: offer,
     details:
-      "We will find you the cheapest price guaranteed! with no train ticket booking fees.",
+      'We will find you the cheapest price guaranteed! with no train ticket booking fees.',
   },
   {
     id: 2,
-    tag_1: "Unified & Multi",
-    tag_2: "Platform Solution",
+    tag_1: 'Unified & Multi',
+    tag_2: 'Platform Solution',
     icon: ticket,
-    details: "with just a few clicks you will get your ticket",
+    icons_2: pass,
+    details: 'with just a few clicks you will get your ticket',
   },
   {
     id: 3,
-    tag_1: "Considered the top",
-    tag_2: "Solution in the industry",
+    tag_1: 'Considered the top',
+    tag_2: 'Solution in the industry',
     icon: wallet,
-    details: "We are giving many opnions",
+    icons_2: pay,
+    details: 'We are giving many opnions',
   },
 ];
